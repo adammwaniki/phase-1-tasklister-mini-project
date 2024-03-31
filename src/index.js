034d13d1b4fe9ff9b1737bb2573b7cca8fd8a948
Adam Mwaniki <adamndegwa@protonmail.com>
2024-03-31 03:23:55 +0300

    Creates functionality for a user to input todo list values

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+//code here
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let newTaskInput = document.getElementById("new-task-description");
+    createNewToDo(newTaskInput.value);
+    form.reset();
+  });
 });
+
+function createNewToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x';
+  li.textContent = `${todo}  `;
+  li.appendChild(btn);
+  document.getElementById('tasks').appendChild(li);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
